517443311925bc6c14ce5ea5d8e57aca658be443
QuentinPhilipp <quentin@itsnotthatkind.org>
2020-03-06 14:40:27 +0100

    Fix missing days

--- Code.js
+++ Code.js
@@ -65,14 +65,13 @@ function getSchema() {
 
 function getDataFromAPI(requestedMetric,startDate,endDate,pageToken,requestEndpoint)
 {
-  var startDateMs = new Date(startDate).getTime() / 1000;
-  var endDateMs = new Date(endDate).getTime() / 1000;
-  
-  var baseUrl = requestEndpoint+"insights?metric=";
-
-  // If date range < 90 days
   const maxTimeDifference = 90*24*60*60;
   const oneDay = 24*60*60;
+  
+  var startDateMs = (new Date(startDate).getTime() / 1000) - oneDay;
+  var endDateMs = (new Date(endDate).getTime() / 1000) + oneDay;
+  
+  var baseUrl = requestEndpoint+"insights?metric=";
   
   if(endDateMs-startDateMs > maxTimeDifference)
   {
@@ -90,21 +89,24 @@ function getDataFromAPI(requestedMetric,startDate,endDate,pageToken,requestEndpo
       
       // if list empty push the complete response
       if(apiresponse.length == 0) {
-        console.log('if');
         apiresponse = JSON.parse(UrlFetchApp.fetch(custom_url));
       }
-      else 
+      else // just append to the values list
       {
-        console.log('else');
         apiresponse.data[0].values = apiresponse.data[0].values.concat(JSON.parse(UrlFetchApp.fetch(custom_url)).data[0].values);
       }
       startIntervalDate = startIntervalDate + maxTimeDifference - oneDay;
       endIntervalDate = endIntervalDate + maxTimeDifference - oneDay;
-      console.log(apiresponse.data[0].values);
+      //console.log(apiresponse.data[0].values);
 
     }
-    console.log(apiresponse);
-    console.log(apiresponse.data[0].values);
+    
+    //rest of the day
+    var custom_url = baseUrl + requestedMetric + "&period=day&since="+startIntervalDate +"&until="+endDateMs +"&access_token="+pageToken;
+    //console.log(custom_url);
+    apiresponse.data[0].values = apiresponse.data[0].values.concat(JSON.parse(UrlFetchApp.fetch(custom_url)).data[0].values);
+    //console.log(apiresponse);
+    //console.log(apiresponse.data[0].values);
 
     
     return apiresponse;
@@ -116,9 +118,7 @@ function getDataFromAPI(requestedMetric,startDate,endDate,pageToken,requestEndpo
     
     var resp = UrlFetchApp.fetch(custom_url);
     var apiresponse =  JSON.parse(resp);
-    console.log(custom_url);
-    console.log(resp);
-    console.log(apiresponse);
+    //console.log(apiresponse.data[0].values);
     return apiresponse;
   }
 
